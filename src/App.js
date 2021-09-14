5ec87aa76fc0145e903988d5820632e53ffbe104
Srinithi-S-2001 <sec19cs031@sairamtap.edu.in>
2021-09-14 21:28:00 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react';
+import './App.scss';
+import {quotes,colors} from './quotes.js';
 function App() {
+  const [quote, setQuote] = useState("I you do what you’ve always done, you’ll get what you’ve always gotten.");
+  const [author, setAuthor] = useState("Tony Robbins");
+  const [randomNumber, setRandomNumber] = useState(0);
+  const[accentColor, setAccentColor] = useState('#282c34');
+  const changeQuoteAndAuthor =()=>{
+    const rand=Math.floor(Math.random()*quotes.length)
+    setQuote(quotes[rand].quote);
+    setAuthor(quotes[rand].author);
+    setAccentColor(colors[rand]);
+    setRandomNumber(rand);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" >
+      <header className="App-header" style={{backgroundColor:accentColor,color:accentColor}}>
+        <div id="quote-box" style={{color:accentColor}}>
+          <p id="text">
+          <i class="fa fa-quote-left">&nbsp;&nbsp;</i>{quote}
+          </p>
+          <p id="author">
+            -{author}
+          </p>
+          <a id="tweet-quote" style={{backgroundColor:accentColor}} href={encodeURI(`https://twitter.com/intent/tweet?text=${quote}-${author}`)}><i class="fa fa-twitter"></i></a>
+          <button style={{backgroundColor:accentColor}} onClick={()=>changeQuoteAndAuthor()} id="new-quote">New Quote</button>
+        </div>
       </header>
     </div>
   );
